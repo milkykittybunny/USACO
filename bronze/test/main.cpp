484fdbab9adcdd871b3f0275f1387f64abc4c6d8
kitybunny <melody.yue.yu@gmail.com>
2018-09-05 15:51:16 -0700

    Updated template

---
+++ bronze/test/main.cpp
@@ -0,0 +1,17 @@
+//C++ template for all USACO codes
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ifstream fIn("test.in");
+    ofstream fOut("test.out");
+
+    // solution comes here
+    int n;
+    fIn >> n;
+
+    fOut << n;
+
+    fIn.close();
+    fOut.close();
+}
--- code_gems.cpp
+++ code_gems.cpp
@@ -9,14 +9,22 @@ git push -u origin master
 
 */
 
-//use this standard C++ template for all codes
-
+//C++ template for all USACO codes
 #include <bits/stdc++.h>
 using namespace std;
 
 int main() {
-  // solution comes here
+    ifstream fIn(".in");
+    ofstream fOut(".out");
 
+    // solution comes here
+    int n;
+    fIn >> n;
+
+
+
+    fIn.close();
+    fOut.close();
 }
 
 
@@ -25,6 +33,32 @@ int main() {
 vector<int> v;
 v.push_back(3); // [3]
 v.push_back(2); // [3,2]
+cout << v.size();
+vector<int> v.resize(10);
+
+//vector insert
+std::vector<int>::iterator it;
+for (int i=1; i<=3; i++) myvector.push_back(i);
+myvector.insert (myvector.begin() + 2 , 200 );
+
+//vector erase
+std::vector<int> myvector;
+// set some values (from 1 to 10)
+for (int i=1; i<=10; i++) myvector.push_back(i);
+// erase the 6th element
+myvector.erase (myvector.begin()+5);
+// erase the first 3 elements:
+myvector.erase (myvector.begin(),myvector.begin()+3);
+
+//vector iteration
+for (auto it = myvector.begin() ; it != myvector.end(); ++it)
+  cout << ' ' << *it;
+//vector reverse iteration
+for (auto rit = myvector.rbegin() ; rit != myvector.rend(); ++rit)
+  cout << ' ' << *rit;
+
+
+
 
 //set
 set<int> s;
@@ -50,6 +84,9 @@ if(!s.empty())
     cout << *s.rbegin(); //pay attention to the "*"
 else
     //the set is empty
+
+
+
 
 //std::map<key, value> map;
 map<string,int> m;
