b79221f433cdc9080dc4c9ee791d7a5b403e147a
melodyyu <melody.yue.yu@gmail.com>
2018-12-02 19:45:12 -0800

    added old questions

---
+++ bronze/11_nov_pageant/main.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ifstream fIn("pageant.in");
+    ofstream fOut("pageant.out");
+
+    // solution comes here
+    int n;
+    fIn >> n;
+
+
+
+    fIn.close();
+    fOut.close();
+}
